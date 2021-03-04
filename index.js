ca8386b81d396f7aa9b12efd7eb1d6543e3f1f73
Giovany <giochavez320@gmail.com>
2021-03-04 16:58:42 -0700

    Addition of user questions in index.js

--- index.js
+++ index.js
@@ -4,7 +4,55 @@ const fs = require('fs')
 const genMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Enter a short description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Enter installation instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Enter usage information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Enter contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Enter test instructions for your project.',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'What license would you like for your project?',
+        choices: [ 'Choice A', 'Choice B', 'Choice C', 'Choice D' ],
+    },
+    {
+        type: 'input',
+        name: 'Username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your email?',
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
